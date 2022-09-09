2c058c016b3a3fce46d2d0d7ebfe78c374c59819
jakergrossman <jake.r.grossman@gmail.com>
2022-09-09 00:24:25 -0500

    add snippets, glslview

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -1,9 +1,17 @@
 return require('packer').startup(function(use)
+  vim.cmd([[
+    augroup packer_user_config
+      autocmd!
+      autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+    augroup end
+  ]])
+
 
   use 'wbthomason/packer.nvim'
 
   use 'neovim/nvim-lspconfig'
-
+ 
+  use 'jose-elias-alvarez/nvim-lsp-ts-utils'
   use 'jose-elias-alvarez/null-ls.nvim'
 
   use 'airblade/vim-gitgutter'
@@ -16,6 +24,7 @@ return require('packer').startup(function(use)
   vim.g.polyglot_disabled = {'sensible', 'markdown'}
   use 'sheerun/vim-polyglot'
 
+  vim.g.tagurl_enable_mapping = false;
   use 'jakergrossman/tagurl.vim'
 
   use 'lukas-reineke/indent-blankline.nvim'
@@ -30,11 +39,39 @@ return require('packer').startup(function(use)
 
   use 'tikhomirov/vim-glsl'
 
-  vim.cmd([[
-    augroup packer_user_config
-      autocmd!
-      autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-    augroup end
-  ]])
+  use {
+    'norcalli/snippets.nvim',
+    config = function()
+      -- keybindings
+      vim.api.nvim_set_keymap("i", "<C-k>", "<cmd>lua return require('snippets').expand_or_advance(1)<CR>", { noremap=true })
+      vim.api.nvim_set_keymap("i", "<C-j>", "<cmd>lua return require('snippets').advance_snippet(-1)<CR>", { noremap=true })
+
+      -- snippet definitions
+      time = function() return os.date("%H:%M:%S") end
+      snippets = require('snip')
+      require('snippets').snippets = require('snip')
+    end
+  }
+
+  use {
+    '/home/jgrossman/code/glslView-nvim',
+    ft = 'glsl',
+    config = function()
+      local size = 512;
+      local top = 75;
+      local right = 25;
+      require('glslView').setup({
+          exe_path = '/mnt/c/Users/jake/Documents/code/glslViewer/MinSizeRel/glslViewer.exe',
+          args = {
+            '-l',
+            '--fps', 0 ,
+            '--nocursor' ,
+            '-w', size,
+            '-h', size,
+            '-x', 1920 - size - right,
+            '-y', top },
+      })
+    end
+  }
 
 end)
