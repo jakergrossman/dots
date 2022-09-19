5be2bf5deab98534edc1c3e11e125744db49e0a9
jakergrossman <jake.r.grossman@gmail.com>
2022-09-18 19:41:01 -0500

    fish shell personalizations
    use material darker colors
    
    replace ufetch with colorful friends
    
    rainbow script

--- shell/.config/fish/config.fish
+++ shell/.config/fish/config.fish
@@ -39,21 +39,47 @@ set -x LESS_TERMCAP_ue \e'[0m'           # end underline
 set -x LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
 
 function fish_prompt
-    set_color brblack
+    set_color $material_gray brblack
     echo -n "["(date "+%H:%M")"] "
 
-    set_color magenta
+    set_color $material_darkpurple magenta
     echo -n (basename $PWD)
 
-    set_color yellow
+    set_color $material_darkgreen green
     printf '%s ' (__fish_git_prompt)
-    set_color red
+    set_color $material_red red
     echo -n '| '
-    set_color normal
+    set_color $material_white normal
 end
 
 function fish_greeting
-    if type -q ufetch
-        ufetch
+    if type -q colorscript
+        set -l goodscripts 8 11 14 48 56
+        set -l pick (random 1 (count $goodscripts))
+        colorscript --exec $goodscripts[$pick]
     end
+
+    set_color $material_gray
+    echo -n 'OS:     '
+    set_color normal
+    lsb_release -ds | rainbow
+
+    set_color $material_gray
+    echo -n 'UPTIME: '
+    set_color normal
+    uptime -p | string replace 'up ' '' | rainbow
+
+    echo '--------------------------' | rainbow
 end
+
+# color settings
+set  -g  fish_color_error        $material_error
+set  -g  fish_color_command      $material_darkblue
+set  -g  fish_color_operator     $material_darkcyan
+set  -g  fish_color_param        $material_blue
+set  -g  fish_color_quote        $material_darkgreen
+set  -g  fish_color_end          $material_darkpurple
+set  -g  fish_color_cwd          $material_cyan
+set  -g  fish_color_escape       $material_orange
+set  -g  fish_color_normal       $material_white
+set  -g  fish_color_redirection  $material_paleblue
