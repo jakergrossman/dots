f1c0c9cd9dc4e9b256114587a403bdc03113fb19
jakergrossman <jake.r.grossman@gmail.com>
2023-06-21 23:43:24 -0500

    fish: use exa instead of ls when installed

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,13 @@
 # just bootstrap conf.d/
 set -x fish_function_path "$__fish_config_dir/conf.d/functions" $fish_function_path
 set -x fish_complete_path "$__fish_config_dir/conf.d/completions" $fish_complete_path
+
+fish_add_path ~/.local/bin
+fish_add_path ~/.cargo/bin
+
+if type -q exa
+    set ls exa
+else
+    set ls ls
+end
+abbr -a l $ls
