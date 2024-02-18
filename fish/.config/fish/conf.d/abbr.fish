2306927a1e3f4458f72f37e68accf035aed24a78
jakergrossman <jake.r.grossman@gmail.com>
2024-02-18 12:39:00 -0600

    prefer "eza" over "exa" over "ls"

--- fish/.config/fish/conf.d/abbr.fish
+++ fish/.config/fish/conf.d/abbr.fish
@@ -1,9 +1,11 @@
 # general aliases that don't fit anywhere else
 
-if type -q exa
+if type -q eza
+    set ls eza
+else if type -q exa
     set ls exa
 else
     set ls ls
 end
 abbr -a l $ls
-
+abbr -a ls $ls
