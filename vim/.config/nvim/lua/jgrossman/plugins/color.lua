4e2d4ea4d1944b78247bc803ac5599ff4aab1a6d
jakergrossman <jake.r.grossman@gmail.com>
2024-09-21 01:15:13 -0500

    vim: remove unused visual plugins

--- vim/.config/nvim/lua/jgrossman/plugins/color.lua
+++ vim/.config/nvim/lua/jgrossman/plugins/color.lua
@@ -10,30 +10,5 @@ return {
             vim.o.listchars = "space:\u{00B7},tab:\u{00BB} "
         end,
     },
-	{
-		"jmckiern/vim-shoot",
-		build = '"./install.py" geckodriver',
-	},
-	{
-		"sapegin/squirrelsong",
-		dir = "~/projects/squirrelsong",
-		config = function(plugin)
-			vim.opt.rtp:append(plugin.dir .. "/light/Neovim")
-		end,
-	},
 	"blazkowolf/gruber-darker.nvim",
-	{
-		'Verf/deepwhite.nvim',
-		dir = "~/projects/deepwhite.nvim/",
-		opts = {
-			low_blue_light = false,
-			color_overrides = {
-				['@lsp.type.variable.c'] = { bg = "#FAD4D4" },
-				['@lsp.type.parameter.c'] = { bg = "#FAD4D4" },
-				['@function.call.c'] = { bg = "#EDD4FA" },
-				['@lsp.type.function.c'] = { bg = "#EDD4FA" },
-				Whitespace = { fg = "#CCCBC6" },
-			},
-		},
-	},
 }
