c11880ba57e1e94433bcf111ae62b9caa2dae0e1
jakergrossman <jake.r.grossman@gmail.com>
2024-09-21 01:14:47 -0500

    fish: change prompt

--- fish/.config/fish/conf.d/fish_prompt.fish
+++ fish/.config/fish/conf.d/fish_prompt.fish
@@ -3,50 +3,45 @@
 function fish_prompt
     set laststatus $status
 
-    set user_color $jg_br_red
-    set suffix "\$"
+    set -l brred (set_color brred)
+    set -l brblue (set_color brblue)
+    set -l green (set_color brgreen)
+    set -l normal (set_color normal)
+    set -l yellow (set_color yellow)
+    set -l bryellow (set_color bryellow)
+    set -l brblack (set_color brblack)
+    set -l brpurple (set_color brpurple)
+
+    # datetime / separator line
+    set -l date (date "+%H:%M:%S")
+    set -l sep (string pad -r --char='-' --width=80 -- "- ($date) -")
+    echo -e "$brblack$sep$normal"
+
+    # main line
+    set host ''
+    if test -n "$SSH_TTY"
+        set host "$(prompt_hostname)"
+    end
+
+    set caret '$'
+    set caret_color $yellow
     if fish_is_root_user
-        set user_color $jg_red
-        set suffix "#"
+        set caret '#'
+        set caret_color $brred
     end
 
-    set abbr_dir (realpath --relative-base "$HOME/" --strip (pwd))
-    if test (pwd) = "$HOME"
-        # otherwise, output is "."
-        set abbr_dir "$HOME"
+    set prompt_status (__fish_print_pipestatus "[" "]" "|" "$(set_color red)" "" "$laststatus")
+    if test -n "$host"
+        echo -e -n "$brblue$host$normal:"
     end
+    echo -e -n "$yellow$USER$normal"
+    echo -e -n " "
 
-    set status_color "$(set_color $jg_white --background $jg_red)"
-    set prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "" $laststatus)
+    set -l pwd $(prompt_pwd --dir-length=8 --full-length-dirs=3)
+    set -l vcs $(fish_vcs_prompt | cut -c 2-)
 
-    set njobs (jobs | count)
-    switch $njobs
-        case 0
-            set job_status ""
-        case 1
-            set job_status " ⚡"
-        case '*'
-            set job_status " ⚡($njobs)"
-    end
-
-    if ! test -z "$prompt_status"
-        echo -e -s -n \
-            (set_color --background $jg_red) \
-            "$prompt_status" \
-            (set_color normal) \
-            " "
-    end
-
-    echo -e -s -n \
-        (set_color normal --background $user_color) "$USER"                             \
-        (set_color normal --background $jg_br_red) "::" \
-        (set_color normal --background $jg_br_red)    (prompt_hostname)                 \
-        (set_color normal) " " \
-        (set_color normal --background $jg_br_magenta) "$abbr_dir"                         \
-        (set_color normal) " " \
-        (set_color normal --background $jg_br_green)   (fish_vcs_prompt | cut -c 2-)   \
-        (set_color normal normal)      "\n"                                \
-        (set_color normal --background $jg_br_orange) "["(date "+%H:%M")"]"               \
-        (set_color normal --background $jg_br_yellow)  "$job_status"                       \
-        (set_color normal)      " $suffix "
+    echo -e -n "$bryellow$pwd$normal "
+    test -n "$vcs" && echo -e -n "$brpurple$vcs$normal "
+    test -n "$prompt_status" && echo -e -n "$prompt_status "
+    test -n "$caret" && echo -e -n "$caret_color$caret$normal "
 end
