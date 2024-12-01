0ad1a6f5842e3b51fe41a63539a8723629387ab6
Jacob Grossman <jake.r.grossman@gmail.com>
2024-12-01 01:25:06 -0600

    gruber

--- term/.config/wezterm/wezterm.lua
+++ term/.config/wezterm/wezterm.lua
@@ -8,9 +8,14 @@ if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 
-ui.apply_config(config)
-mapping.apply_config(config)
+-- ui.apply_config(config)
+-- mapping.apply_config(config)
 
+local wez = require("wezterm")
+config.font = wez.font("JetBrains Mono NL")
+config.font_size = 15.0
+config.enable_tab_bar = false
+config.color_scheme = "gruber-darker"
 config.check_for_updates = false
 
 return config
